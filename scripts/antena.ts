101a102110c7279f8faea07a3564bbcb4be73dfc
Thang Le <jakethecat21@gmail.com>
2022-04-13 15:22:20 +0700

    bridge v1

--- scripts/antena.ts
+++ scripts/antena.ts
@@ -1,51 +1,67 @@
 import { BigNumber, ethers } from "ethers";
+import { NetworkBridge } from "../typechain";
 import { BSC_NETWORK, BSC_PROVIDER, RINKEBY_NETWORK, RINKEBY_PROVIDER } from "./networks";
-import { convertBnToDecimal, getWalletByPK } from "./utils";
+import { burnTokenFromBridgeAcc, convertBnToDecimal, getWalletByPK, mintTokenToSender } from "./utils";
 
 const { TESTNET_STAKING_ADDR_PRIVATE_KEY: ADMIN_BRIDGE_PK } = process.env;
 
 const rinkebyBridgeAdmin = getWalletByPK(ADMIN_BRIDGE_PK as string, RINKEBY_PROVIDER);
 const bscBridgeAdmin = getWalletByPK(ADMIN_BRIDGE_PK as string, BSC_PROVIDER);
 
+
+// On network A, user transfer token to the bridge
+// A listener signals the bridge's admin calling the bridge contract to burn the transfered token
+// A Burn event from network A is emitted
+// A listener signals the bridge's admin of network B to mint the burnt amount to the user
+// A mint event is emitted
+
+// BEGIN HANDLING ON RINKEBY
+
 RINKEBY_NETWORK.tokenContract.on(
     'Transfer',
-    async (_from: string, _to: string, _value: BigNumber, _event: ethers.Event ) => {
+    async (_from: string, _to: string, _value: BigNumber) => {
         if (_to !== RINKEBY_NETWORK.bridgeContract.address) return;
-        // On rinkeby network, Bridge admin calling the bridge contract to burn token
-        // A Burn event from Rinkeby network is emitted
-        // Upon Burn event in network rinkeby, a listener trigger
-        // On BSC network, bridge admin calling mint to mint burned amount to the burner
         console.log(`Rinkeby bridge contract received ${convertBnToDecimal(_value)} tokens from ${_from}, attempting to burn`);
-        const bridgeWithSigner = await RINKEBY_NETWORK.bridgeContract.connect(rinkebyBridgeAdmin);
-        const burnTx = await bridgeWithSigner.burn(_from, _value);
-        const receipt = await burnTx.wait();
-        console.log(`Burn tx hash: ${receipt.transactionHash}`)
+        await burnTokenFromBridgeAcc(RINKEBY_NETWORK.bridgeContract, rinkebyBridgeAdmin, {_from, _value})
       }
 );
   
-RINKEBY_NETWORK.bridgeContract.on("Burn",async (_sender: string, _amount: BigNumber) => {
-    console.log(`Successful burn attempt on Rinkeby. ${convertBnToDecimal(_amount)} tokens are burnt from bridge's account`);
+RINKEBY_NETWORK.bridgeContract.on("Burn",async (_sender: string, _value: BigNumber) => {
+    console.log(`Successful burn attempt on Rinkeby. ${convertBnToDecimal(_value)} tokens are burnt from bridge's account`);
     console.log(`Burn event emitted on Rinkeby network, attempting to mint to ${_sender} on BSC network`);
-    const bridgeWithSigner = await BSC_NETWORK.bridgeContract.connect(bscBridgeAdmin);
-    const mintTx = await bridgeWithSigner.mint(_sender, _amount);
-    const receipt = await mintTx.wait();
-    console.log(`Mint tx hash: ${receipt.transactionHash}`)
+
+    await mintTokenToSender(BSC_NETWORK.bridgeContract, bscBridgeAdmin, {_sender, _value});
 })
 
 BSC_NETWORK.bridgeContract.on("Mint",async (_sender: string, _amount: BigNumber) => {
     console.log(`Successful mint attempt on BSC. ${convertBnToDecimal(_amount)} tokens are minted to ${_sender}`);
+    console.log('------------------------------');
+    
 })
 
+// END HANDLING ON RINKEBY
 
-//   const burnToken = (sender: string) => {
+// BEGIN HANDLING ON BSC
 
-//   }
+BSC_NETWORK.tokenContract.on(
+    'Transfer',
+    async (_from: string, _to: string, _value: BigNumber, _event: ethers.Event ) => {
+        if (_to !== BSC_NETWORK.bridgeContract.address) return;
+        console.log(`BSC bridge contract received ${convertBnToDecimal(_value)} tokens from ${_from}, attempting to burn`);
+        await burnTokenFromBridgeAcc(BSC_NETWORK.bridgeContract, bscBridgeAdmin, {_from, _value})
+      }
+);
 
-/**
- * 1. User transfer to bridge
- * 2. Listener listens to bridge got transfered, with amount
- * 3. Bridge's admin call bridge to burn the transfered amount from bridge's account
- * 4. Burn successful, a `Bridge` event is emitted
- * 5. Listener => bridge's admin (other chain) calls mint function from the bridge contract to the sender
- * 
- */
+BSC_NETWORK.bridgeContract.on("Burn",async (_sender: string, _value: BigNumber) => {
+    console.log(`Successful burn attempt on BSC. ${convertBnToDecimal(_value)} tokens are burnt from bridge's account`);
+    console.log(`Burn event emitted on BSC network, attempting to mint to ${_sender} on Rinkeby network`);
+
+    await mintTokenToSender(RINKEBY_NETWORK.bridgeContract, rinkebyBridgeAdmin, {_sender, _value});
+})
+
+RINKEBY_NETWORK.bridgeContract.on("Mint",async (_sender: string, _amount: BigNumber) => {
+    console.log(`Successful mint attempt on Rinkeby. ${convertBnToDecimal(_amount)} tokens are minted to ${_sender}`);
+    console.log('------------------------------');
+})
+
+// END HANDLING ON BSC
--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,5 +1,6 @@
 import { InfuraProvider, JsonRpcProvider } from "@ethersproject/providers";
 import { BigNumber, ethers } from "ethers";
+import { NetworkBridge } from "../typechain";
 
 export const getWalletByPK = (
   PK: string,
@@ -15,3 +16,27 @@ export const convertDecimalToBn = (input: string) => {
   const sanitizedInput = input.replace(/[^\d.-]/g, ""); //https://stackoverflow.com/questions/1862130/strip-all-non-numeric-characters-from-string-in-javascript
   return ethers.utils.parseUnits(sanitizedInput);
 };
+
+
+interface IBurnData {
+  _from: string,
+  _value: BigNumber
+}
+export const burnTokenFromBridgeAcc = async (bridgeContract: NetworkBridge, bridgeAdmin: ethers.Wallet, {_from, _value}: IBurnData) => {
+  const bridgeWithSigner = await bridgeContract.connect(bridgeAdmin);
+  const burnTx = await bridgeWithSigner.burn(_from, _value);
+  const receipt = await burnTx.wait();
+  console.log(`Burn tx hash: ${receipt.transactionHash}`)
+};
+
+interface IMintData {
+  _sender: string,
+  _value: BigNumber
+}
+
+export const mintTokenToSender = async (bridgeContract: NetworkBridge, bridgeAdmin: ethers.Wallet, {_sender, _value}: IMintData) => {
+  const bridgeWithSigner = await bridgeContract.connect(bridgeAdmin);
+  const mintTx = await bridgeWithSigner.mint(_sender, _value);
+  const receipt = await mintTx.wait();
+  console.log(`Mint tx hash: ${receipt.transactionHash}`)
+}
