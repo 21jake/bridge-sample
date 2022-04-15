ce37a7092e27a3f8731fbda22f685ff10fe06a85
Thang Le <jakethecat21@gmail.com>
2022-04-15 15:58:59 +0700

    bridge v2 contract

--- contracts/NetworkBridge.sol
+++ contracts/NetworkBridge.sol
@@ -7,18 +7,35 @@ contract NetworkBridge {
     address public admin;
     address public immutable tokenContract;
 
-    event Burn (address sender, uint256 amount);
+    mapping (address => uint256) public locked;
+
+    event Burn (address sender, uint256 amount, uint256 chainId);
     event Mint (address sender, uint256 amount);
+    event Lock (address sender, uint256 amount);
 
     constructor(address token) {
         admin = msg.sender;
         tokenContract = token;
     }
 
-    function burn (address sender, uint256 amount) external onlyAdmin {
+    function lock (address sender, uint256 amount) external onlyAdmin {
+        locked[sender] += amount;
+        emit Lock(sender, amount);
+    }
+
+    function burn (uint256 amount, uint chainId) external {
+        require(locked[msg.sender] >= amount, "Bridge: Insufficient locked amount");
+        locked[msg.sender] -= amount;
         bool success = Token(tokenContract).burn(amount);
         require(success, "Unsucessful burn attempt");
-        emit Burn(sender, amount);
+        emit Burn(msg.sender, amount, chainId);
+    }
+
+    function withdraw (uint256 amount) external {
+        require(locked[msg.sender] >= amount, "Bridge: Insufficient locked amount");
+        locked[msg.sender] -= amount;
+        bool success = Token(tokenContract).transfer(msg.sender, amount);
+        require(success, "Unsucessful withdraw attempt");
     }
 
     function mint (address burner, uint256 amount) external onlyAdmin {
